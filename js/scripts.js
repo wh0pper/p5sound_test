f644b3962fecac491d88e366451cc972a0ff8b02
Jared Clemmensen <Guest@Epicodus-9C.local>
2018-02-26 18:54:49 -0800

    implement drop_check and create_drops function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
+function drop_check(drop) {
+  if (drop.position.y < 0) {
+    drop.position.y =  Math.floor(Math.random() * (20 - 1)) + 1;
+  }
+}
+
+function create_drops(num) {
+  var dropGeo = new THREE.CylinderGeometry(.1, .1, 5);
+  var dropMat = new THREE.MeshLambertMaterial( {color: 0xa0c4ff} );
+  for(var i = 0; i < num; i++) {
+    var drop = new THREE.Mesh( dropGeo, dropMat );
+    var posX = Math.floor(Math.random() * (10 - 1)) + 1;
+    var posZ = Math.floor(Math.random() * (10 - 1)) + 1;
+    var posY = 20
+    drop.position.x = posX;
+    drop.position.z = posZ;
+    drop.position.y = posY;
+    drops.add(drop);
+  }
+}
+
 
 var renderer = new THREE.WebGLRenderer();
 
 // Create a scene which will hold all our meshes to be rendered
 var scene = new THREE.Scene();
-
 
 // Create and position a camera
 var camera = new THREE.PerspectiveCamera(
@@ -31,67 +51,29 @@ renderer.setClearColor( 0xfff6e6 );
 // Append to the document
 document.body.appendChild( renderer.domElement );
 
-//A mesh is created from the geometry and material, then added to the scene
-// var plane = new THREE.Mesh(
-// new THREE.PlaneGeometry( 5, 5, 5, 5 ),
-// new THREE.MeshBasicMaterial( { color: 0x393839, wireframe: true } )
-// );
-// plane.rotateX(Math.PI/2);
-// scene.add( plane );
+var drops = new THREE.Group();
 
-//add cube to scene
-// var boxGeo = new THREE.BoxGeometry( 1, 1, 1);
-// var boxMat = new THREE.MeshBasicMaterial ( { color: 0xff69b4 } );
-// var box = new THREE.Mesh( boxGeo, boxMat );
-// scene.add( box );
+create_drops(100);
 
-//add bones
-// var root = new THREE.Bone();
-// var child = new THREE.Bone();
+scene.add(drops);
 
-// root.add( child );
-// child.position.y = 5;
+// Render the scene/camera combination
+renderer.render(scene, camera);
 
+// Add an orbit control which allows us to move around the scene. See the three.js example for more details
+// // https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.addEventListener( 'change', function() { renderer.render(scene, camera); } ); // add this only if there is no animation loop (requestAnimationFrame
 
-  var drops = new THREE.Object3D();
+//scene animate
+function animate() {
+  requestAnimationFrame( animate );
 
-  var dropGeo = new THREE.CylinderGeometry(.1, .1, 10);
-  var dropMat = new THREE.MeshLambertMaterial( {color: 0xa0c4ff} );
-
-  for(var i = 0; i < 10; i++) {
-    var drop = new THREE.Mesh( dropGeo, dropMat );
-    var posX = Math.floor(Math.random() * (10 - 1)) + 1;
-    var posZ = Math.floor(Math.random() * (10 - 1)) + 1;
-
-    drop.position.x = posX;
-    drop.position.z = posZ;
-    drop.position.y = 0;
-    drops.add(drop);
-    scene.add(drop);
-
+  drops.children.forEach(function(drop) {
+    drop.position.y -= 2;
+    drop_check(drop)
+    console.log(drop.position.y)
+  })
+  renderer.render (scene, camera);
 }
-
-
-
-  // Render the scene/camera combination
-  renderer.render(scene, camera);
-
-  // Add an orbit control which allows us to move around the scene. See the three.js example for more details
-  // // https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.
-  var controls = new THREE.OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', function() { renderer.render(scene, camera); } ); // add this only if there is no animation loop (requestAnimationFrame
-
-
-  //scene animate
-  function animate() {
-
-    requestAnimationFrame( animate );
-    drops.position.y += 0.01;
-    // drops.position.y += 1;
-  // scene.rotation.x += 0.01;
-    // scene.rotation.y += 0.01;
-
-    renderer.render (scene, camera);
-  }
-
-  animate();
+animate();
---
+++ js/simflora_ex.js
@@ -0,0 +1,143 @@
+function Soil() {
+	var topPoints = [];
+	for ( var i = 10; i >= 0; i-- ) {
+		topPoints.push(new THREE.Vector2(i, Math.cos(Math.PI/20*i)));
+	}
+	var bottomPoints = [];
+	for ( var i = 0; i <= 10; i++ ) {
+		bottomPoints.push(new THREE.Vector2(i, -2*(1+Math.cos(Math.PI/10*i))));
+	}
+
+	var topGeometry = new THREE.LatheGeometry( topPoints, 6);
+	var topMaterial = new THREE.MeshLambertMaterial( { color: 0x344b37 } );
+
+	this.top = new THREE.Mesh( topGeometry, topMaterial );
+	this.top.castShadow = true;
+	this.top.receiveShadow = true;
+
+	var rimGeometry = new THREE.TorusGeometry(10, .5, 16, 6);
+
+	this.rim = new THREE.Mesh(rimGeometry, topMaterial);
+	this.rim.rotation.x = Math.PI/2;
+	this.rim.rotation.z = Math.PI/2;
+	this.rim.position.y = -.5;
+
+	this.group = new THREE.Group();
+	this.stoneSnow = new THREE.Group();
+	this.groundSnow = new THREE.Group();
+	this.stoneMoss = new THREE.Group();
+
+	this.group.add(this.stoneSnow);
+	this.group.add(this.stoneMoss);
+	this.group.add(this.groundSnow);
+
+	var snowMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+	this.snowTop = new THREE.Mesh( topGeometry, snowMaterial );
+	this.snowTop.position.y = -.05;
+	// this.snowTop.castShadow = true;
+	// this.snowTop.receiveShadow = true;
+
+
+	var snowRimGeometry = new THREE.TorusGeometry(10, .45, 16, 6);
+	this.snowRim = new THREE.Mesh(snowRimGeometry, snowMaterial);
+	this.snowRim.rotation.x = Math.PI/2;
+	this.snowRim.rotation.z = Math.PI/2;
+	this.snowRim.position.y = -.5;
+	this.groundSnow.add(this.snowTop);
+	this.groundSnow.add(this.snowRim);
+
+	var bottomGeometry = new THREE.LatheGeometry( bottomPoints, 6 );
+	var bottomMaterial = new THREE.MeshLambertMaterial( { color: 0x917054 } );
+	this.bottom = new THREE.Mesh( bottomGeometry, bottomMaterial );
+	this.bottom.position.y = -.8;
+	this.bottom.castShadow = true;
+	//bottom.receiveShadow = true;
+
+	this.group.add(this.top);
+	this.group.add(this.rim);
+	this.group.add(this.bottom);
+
+	var material = new THREE.MeshLambertMaterial( {color: 0x423224} );
+
+	for (var i = 0; i < 90; i++) {
+		var size = Math.random()*.7 + .25;
+		var geometry = new THREE.BoxGeometry(size, size, size);
+		var x = 1 + Math.random() * 6;
+		geometry.translate(x, 0, 0);
+		var clod = new THREE.Mesh(geometry, material);
+		clod.position.y = -3*(1+Math.cos(Math.PI/10*x))*Math.random() - .5;
+		clod.quaternion.setFromEuler(rotateAndTilt(Math.random() * 90, Math.random() * 360));
+		this.group.add(clod);
+		//clod.castShadow = true;
+		clod.receiveShadow = true;
+
+	};
+
+	var stoneMaterial = new THREE.MeshLambertMaterial( {color: 0x656b59} );
+	var mossMaterial = new THREE.MeshLambertMaterial( {color: 0x4d5e35} );
+
+	for (var i = 0; i < 8; i++) {
+		var radius = Math.random() + .5;
+		var stoneGeometry = new THREE.SphereGeometry(radius, 8, 8 );
+		var snowGeometry = new THREE.SphereGeometry(radius*.9, 8, 8);
+		var mossGeometry = new THREE.SphereGeometry(radius*.95, 8, 8);
+
+		var x = 2 + Math.random() * 6.5;
+		stoneGeometry.translate(x, 0, 0);
+		snowGeometry.translate(x, 0, 0);
+		mossGeometry.translate(x, 0, 0);
+
+		var stone = new THREE.Mesh( stoneGeometry, stoneMaterial );
+		var snow = new THREE.Mesh(snowGeometry, snowMaterial);
+		var moss = new THREE.Mesh(mossGeometry, mossMaterial);
+
+		// stone.castShadow = true;
+
+		position = Math.cos(Math.PI/20*x);
+		eu = rotateAndTilt(Math.random() * 90, Math.random() * 360);
+
+		stone.position.y = position;
+		snow.position.y = position;
+		moss.position.y = position;
+
+		stone.quaternion.setFromEuler(eu);
+		snow.quaternion.setFromEuler(eu);
+		moss.quaternion.setFromEuler(eu);
+
+		stone.scale.set(1, .5, 1);
+		snow.scale.set(1, .5, 1);
+		moss.scale.set(1, .5, 1);
+
+		this.group.add(stone);
+		this.stoneSnow.add(snow);
+		this.stoneMoss.add(moss);
+	};
+
+	this.stoneMoss.position.y = .07
+	this.group.scale.set(.05, .05, .05);
+
+
+	//, ,
+	// this.winterTween = new TWEEN.Tween(this.top.material.color).to({r: 1, g: 1, b: 1 }, 500);
+	// this.springTween = new TWEEN.Tween(this.top.material.color).to({r: 0.314, g: 0.522, b: 0.322 }, 1000);
+};
+
+Soil.prototype.update = function(time) {
+
+
+	var snowHeight = -Math.sin(time.seasonRad)*.3;
+	if (snowHeight > 0) {
+		this.stoneSnow.position.y = -Math.sin(time.seasonRad)*.4;
+		this.groundSnow.position.y = -Math.sin(time.seasonRad)*.09;
+		this.groundSnow.scale.set(1, 1 + snowHeight*.2, 1)
+
+	}
+
+	// if (time.currentSeason === "WI" && time.lastSeason === "FA") {
+	// 	this.springTween.stop();
+	// 	this.winterTween.start();
+	// } else if (time.currentSeason === "SP" && time.lastSeason === "WI") {
+	// 	this.winterTween.stop();
+	// 	this.springTween.start();
+	// }
+}
