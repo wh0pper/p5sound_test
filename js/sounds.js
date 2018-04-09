5dc2f27d54cee1cb7055d731beba91a3b4ee561f
wh0pper <jaredclemmensen@gmail.com>
2018-04-08 19:57:50 -0700

    add shapes and mess with canvase size

--- js/sounds.js
+++ js/sounds.js
@@ -2,11 +2,11 @@ var song;
 var amplitude;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1200, 1000);
   song = loadSound('resonance.mp3', loaded);
   amplitude = new p5.Amplitude();
   frameRate(60);
-  stroke(255);
+
 }
 
 function loaded() {
@@ -19,7 +19,18 @@ function draw() {
   var vol = amplitude.getLevel();
   // line(0,vol*height,width, vol*height);
   // console.log(vol);
+
+  fill(255);
+  stroke(255);
   ellipse(width/2,height/2,500*vol,500*vol);
+
+
+  fill(0);
+  ellipse(width/2,height/2,200*vol,200*vol);
+
+  fill('rgba(0,0,0,0)');
+  stroke(255, 0, 0);
+  ellipse(width/2,height/2,100/vol,100/vol);
   // beginShape();
   // volHistory.push(vol);
   // for (var i = 0; i < volHistory.length; i++) {
